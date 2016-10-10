a564d04b95e037c63e6d4d3802bdef9a043f87ec
moonbrv <moonbrv@gmail.com>
2016-10-10 23:19:42 +0300

    add first test

---
+++ test/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import App from './../assets/js/components/App';
+
+describe('Component: App', () => {
+    /*let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<App/>)
+    });
+    
+    it('must be Rendered', () => {
+        expect(wrapper.isEmpty()).to.be.equal(false);
+    });*/
+    
+    it('myst be true', () => {
+        expect(1).to.be.equal(1);
+    });
+        
+        
+});
